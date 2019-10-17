b25fc4c336e0b06f6cc7c9c1857440689ce785ce
Jonathan Luk <jluk87@gmail.com>
2019-10-17 00:11:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function createEmployeeRecord(arrOfEmployee) {
+  return Object.assign({}, { firstName: arrOfEmployee[0], familyName: arrOfEmployee[1], title: arrOfEmployee[2], payPerHour: arrOfEmployee[3], timeInEvents: [], timeOutEvents: []})
+}
+
+function createEmployeeRecords(arrOfEmployees) {
+  return arrOfEmployees.map(employee => createEmployeeRecord(employee));
+}
+
+function createTimeInEvent(employee, datestamp) {
+  const [date, hour] = datestamp.split(' ');
+  const newTimeInEvent = Object.assign({}, { type: 'TimeIn', hour: parseInt(hour), date: date})
+  employee.timeInEvents.push(newTimeInEvent);
+  return employee;
+}
