8fe795b36d1d2b8395123e241604bb71bd09f369
Rebecca Tong <rtong@buffalo.edu>
2019-12-04 14:40:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+
+function createEmployeeRecord(array){
+ let card = {
+  firstName: array[0],
+  familyName: array[1],
+  title: array[2],
+  payPerHour: array[3],
+  timeInEvents: [],
+  timeOutEvents: []
+}
+return card
+}
