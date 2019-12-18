cf01d42202fc3102e33f5fb544c99c256e7dbe64
nanton83 <nicholas.romans@gmail.com>
2019-12-18 14:20:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+let createEmployeeRecord = function(fourElementArray){
+    return {
+        firstName: fourElementArray[0],
+        familyName: fourElementArray[1],
+        title: fourElementArray[2],
+        payPerHour: fourElementArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
