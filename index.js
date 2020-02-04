d5b11aa707b4f799ca89e627abbc4175c72b0905
daniellecroxton <danikrist@gmail.com>
2020-02-04 15:17:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Your code here
+function createEmployeeRecord(employeeArray) {
+    var employee = {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return employee;
+};
+
+function createEmployeeRecords(arrayOfEmployees) {
+    var employees = arrayOfEmployees.map(employee => createEmployeeRecord(employee));
+    return employees;
+}
+
+function createTimeInEvent(employee, timeIn) {
+    var timeArray = timeIn.split(" ")
+    employee.timeInEvents = [{
+        type: "TimeIn",
+        hour: Number(timeArray[1]),
+        date: timeArray[0]
+    }]
+    return employee;
+}
+
+function createTimeOutEvent(employee, timeOut) {
+    var timeArray = timeOut.split(" ")
+    employee.timeOutEvents = [{
+        type: "TimeOut",
+        hour: Number(timeArray[1]),
+        date: timeArray[0]
+    }]
+    return employee;
+}
+
+function hoursWorkedonDate(employee, dateWorked) {
+
+}
+
+function wagesEarnedOnDate(employee, dateWorked) {
+
+}
+
+function allWagesFor(employee) {
+
+}
+
+function findEmployeeByFirstName(employees, firstName) {
+
+}
