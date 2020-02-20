e6c51b0a1d8fc3d979f78668d7164260eb3be4cb
MiriamPeskowitz <miriam5000@gmail.com>
2020-02-20 13:03:25 -0500

    works on refactor

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+const createEmployeeRecord = (data) => {
+	return {
+	 firstName: data[0],
+	 familyName: data[1],
+	 title: data[2],
+	 payPerHour: data[3],
+	 timeInEvents: [],
+	 timeOutEvents: []
+	}
+}
+
+const createEmployeeRecords = (dataArrays) => {
+	return dataArrays.map(function(row){
+		return createEmployeeRecord(row)
+	})
+}
+
+const createTimeInEvent = (employeeData, dateStamp) => {
+	return dateDivider(dateStamp)
+	// let splitDate = dateStamp.split(' ')
+	// let hour = splitDate[1]
+	// let date = splitDate[0]
+
+	employeeData.timeInEvents.push({
+		type: "TimeIn",
+		hour: hour,
+		date: date,
+	})
+
+	return employeeData
+}
+
+const dateDivider = (dateStamp) => {
+	let splitDate = dateStamp.split(' ')
+	let hour = splitDate[1]
+	let date = splitDate[0]
+	return hour, date
+}
