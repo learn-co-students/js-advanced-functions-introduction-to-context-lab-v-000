20f40e37e53b15cc07f230bb823d865f37890766
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-05 20:30:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+//1) has a function called createEmployeeRecord
+function createEmployeeRecord() {
+
+    this.firstName[0]
+}
+
+//createEmployeeRecord
+//1) populates a firstName field from the 0th element
+//2) populates a familyName field from the 1th element
+//3) populates a title field from the 2th element
+//4) populates a payPerHour field from the 3th element
+//5) initializes a field, timeInEvents, to hold an empty Array
+//6) initializes a field, timeOutEvents, to hold an empty Array
